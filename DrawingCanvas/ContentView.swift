013335e2a10561144663e33718cfba18bacbe368
Ethan Humphrey <ethantheinnovator@gmail.com>
2019-11-11 08:54:02 -0500

    Initial Commit

---
+++ DrawingCanvas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DrawingCanvas
+//
+//  Created by Ethan Humphrey on 11/11/19.
+//  Copyright © 2019 Ethan Humphrey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
